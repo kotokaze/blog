2974b1dd03d33a9a1c03c00d18cefe7f7d4bcb13
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-09-17 23:16:43 +0900

    :sparkles: Setup uikit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import UIKit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+import 'uikit/dist/css/uikit.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => UIKit.use(Icons))
   return <Component {...pageProps} />
 }
 export default MyApp
