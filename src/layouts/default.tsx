78a05ea923d8a47a3ce256a64d96a0d9963bc77d
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-09-28 16:30:05 +0900

    :white_check_mark: Add types

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -2,12 +2,16 @@ import UIkit from '../components/uikit'; UIkit
 import Navbar from '../components/navbar'; Navbar
 import OffCanvases from '../components/offcanvases'; OffCanvases
 
-const Layout = ({ children }) => pug`
+interface Props {
+  children: React.ReactNode
+}
+
+const Layout: React.VFC<Props> = ({ children }) => pug`
   UIkit/
     header
       Navbar/
     main
-      ${children}
+      = children
     footer
     OffCanvases/
 `
