66cdc05430a91e7eb42acc44c69f8105f19fa5ce
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-10-06 03:49:24 +0900

    :warning: Fix typo

--- src/pages/slides/index.hook.ts
+++ src/pages/slides/index.hook.ts
@@ -1,9 +1,9 @@
-import type { GetStaticPathsContext, InferGetStaticPropsType } from 'next'
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
 import apiClient from '@/modules/api-client'
 
 export type Props = InferGetStaticPropsType<typeof getStaticProps>
 
-export const getStaticProps = async (_: GetStaticPathsContext) => {
+export const getStaticProps = async (_: GetStaticPropsContext) => {
   const site: Promise<Site> = apiClient.v1.site.$get()
   const props = await Promise.all([site]).then(([site]) => ({ site }))
 
