14893d9574f3b1cb45582fb7ca5ceee30423d937
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-10-08 02:46:39 +0900

    :sparkles: Add useKey hook

---
+++ src/hooks/useKey.ts
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useRef } from 'react'
+
+interface UseKeyOptions {
+  listen?: boolean
+  eventType?: 'keydown' | 'keypress' | 'keyup'
+}
+
+const useKey = (
+  input: string | number,
+  cb: (e: KeyboardEvent) => any,
+  options: UseKeyOptions = {
+    listen: true,
+    eventType: 'keydown',
+  }
+): void => {
+  const { listen, eventType } = options
+  const cbRef = useRef<(e: KeyboardEvent) => any>(cb)
+  useEffect(() => {
+    cbRef.current = cb
+  })
+
+  const keyListener = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key.match(input.toString())) cbRef.current(e)
+    },
+    [input]
+  )
+
+  useEffect(() => {
+    if (!listen || window === undefined) return
+
+    document.addEventListener(eventType!, keyListener)
+    return () => document.removeEventListener(eventType!, keyListener)
+  }, [keyListener, listen, eventType])
+}
+
+export { useKey }
