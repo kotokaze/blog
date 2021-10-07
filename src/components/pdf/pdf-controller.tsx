479be7b50b3e1d2ea11adda1d4f7f9f2a9c584a4
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-10-08 02:51:33 +0900

    :white_check_mark: Add keyboard page transition

--- src/components/pdf/pdf-controller.tsx
+++ src/components/pdf/pdf-controller.tsx
@@ -1,3 +1,5 @@
+import { useKey } from '@/hooks/useKey'
+
 interface Props {
   isPrevDisabled: boolean
   isNextDisabled: boolean
@@ -5,17 +7,37 @@ interface Props {
   nextPage: () => void
 }
 
-const PageController: React.VFC<Props> = ({ prevPage, nextPage, isPrevDisabled, isNextDisabled }) => pug`
-  .uk-flex.uk-flex-center
-    button(
-      onClick=prevPage
-      disabled=isPrevDisabled
-    ).uk-button.uk-button-default.uk-margin-small-right #[span(data-uk-icon='icon: chevron-double-left')]
-    button(
-      onClick=nextPage
-      disabled=isNextDisabled
-    ).uk-button.uk-button-default #[span(data-uk-icon='icon: chevron-double-right')]
-`
+const PageController: React.VFC<Props> = ({
+  prevPage,
+  nextPage,
+  isPrevDisabled,
+  isNextDisabled,
+}) => {
+  const onEnterPressed = ({ key }: KeyboardEvent) => {
+    if (!isNextDisabled) nextPage()
+  }
+  const onArrowRightPressed = ({ key }: KeyboardEvent) => {
+    if (!isNextDisabled) nextPage()
+  }
+  const onArrowLeftPressed = ({ key }: KeyboardEvent) => {
+    if (!isPrevDisabled) prevPage()
+  }
 
+  useKey('Enter', onEnterPressed)
+  useKey('ArrowRight', onArrowRightPressed)
+  useKey('ArrowLeft', onArrowLeftPressed)
+
+  return pug`
+    .uk-flex.uk-flex-center
+      button(
+        onClick=prevPage
+        disabled=isPrevDisabled
+      ).uk-button.uk-button-default.uk-margin-small-right #[span(data-uk-icon='icon: chevron-double-left')]
+      button(
+        onClick=nextPage
+        disabled=isNextDisabled
+      ).uk-button.uk-button-default #[span(data-uk-icon='icon: chevron-double-right')]
+  `
+}
 
 export default PageController
