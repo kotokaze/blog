46b6dbb60030692aea09c25093e80463923b4046
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-10-19 10:49:56 +0900

    :white_check_mark: Generalize CardList component

--- src/components/card-list.tsx
+++ src/components/card-list.tsx
@@ -1,25 +1,34 @@
 import Link from 'next/link'; Link
 import DateTime from '@/lib/date-time'; DateTime
 
-interface Props {
-  articles: Array<Article>
+interface CardItem extends Content {
+  title: string
+  categories: Array<Category>
+  description: string
+  ogImage: ImageInfo
 }
 
-const CardList: React.VFC<Props> = ({ articles }) => pug`
+interface Props {
+  basePath: string
+  items: Array<CardItem>
+}
+
+const CardList: React.VFC<Props> = ({ basePath, items }) => pug`
   .uk-grid-small.uk-flex-center(data-uk-grid)
-    each blog in articles
-      .uk-card.uk-card-default.uk-margin-right(className='uk-width-1-3@s', key=blog.id)
-        Link(href={pathname: '/blogs/[slug]', query: { slug: blog.id }})
+    each item in items
+      .uk-card.uk-card-default.uk-margin-right(className='uk-width-1-3@s', key=item.id)
+        Link(href={pathname: basePath + '/[slug]', query: { slug: item.id }})
           a.uk-link-toggle.uk-card-header.uk-text-center
-            h4.uk-card-title.uk-text-truncate #[span.uk-link-heading #{blog.title}]
-            img(src=blog.ogImage.url + '?fit=fill&fill-color=white&w=200&h=200' data-uk-image)
+            h4.uk-card-title.uk-text-truncate #[span.uk-link-heading #{item.title}]
+            img(src=item.ogImage.url + '?fit=fill&fill-color=white&w=200&h=200' data-uk-image)
 
         .uk-card-footer.uk-text-meta
-          p #[span(data-uk-icon='calendar')] #[span &nbsp;] #[time(dateTime=blog.publishedAt) #{DateTime.date(blog.publishedAt)}]
+          p #[span(data-uk-icon='calendar')] #[span &nbsp;] #[time(dateTime=item.publishedAt) #{DateTime.date(item.publishedAt)}]
           p #[span(data-uk-icon='tag')] #[span &nbsp;]
-            each cat in blog.categories
+            each cat in item.categories
               Link(href={pathname: '/categories/[slug]', query: { slug: cat.id }}, key=cat.id)
                 a.uk-margin-small-right #[span.uk-badge #{cat.name}]
 `
 
+export type { CardItem }
 export default CardList
--- src/pages/blogs/index.page.tsx
+++ src/pages/blogs/index.page.tsx
@@ -7,11 +7,11 @@ import CardList from '@/components/card-list'; CardList
 import WithSidebar from '@/components/layouts/with-sidebar'; WithSidebar
 import { Props } from './index.hook'
 
-const BlogsPage: NextPage<Props> = ({ articles, site }) => pug`
+const BlogsPage: NextPage<Props> = ({ items, site }) => pug`
   Head
     title 記事一覧 | #{site.title}
   WithSidebar(site=site)
-    CardList(articles=articles)
+    CardList(basePath='/blogs', items=items)
 `
 
 export { getStaticProps } from './index.hook'
