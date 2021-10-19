51b9098d7722f8195e9d51441126d19f3ff924a3
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-10-19 18:51:14 +0900

    :sparkles: Add categories page

---
+++ src/pages/categories/[slug].hook.ts
@@ -0,0 +1,50 @@
+import type { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+import type { CardItem } from '@/components/card-list'
+import microcmsClient from '@/modules/microcms'
+
+export type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths: Array<{ params: { slug: string } }> =
+    await microcmsClient.v1.categories
+      .$get({ query: { fields: 'id' } })
+      .then((res) => res.contents)
+      .then((contents) => contents.map((item) => item.id))
+      .then((ids) => ids.map((id) => ({ params: { slug: id } })))
+
+  return { paths, fallback: false }
+}
+
+export const getStaticProps = async (ctx: GetStaticPropsContext) => {
+  const { slug } = ctx.params!
+
+  const category: Promise<Category> = microcmsClient.v1.categories
+    ._id(slug!.toString())
+    .$get({ query: { fields: 'name' } })
+
+  const query: MethodsGetQuery = {
+    filters: `categories[contains]${slug!.toString()}`,
+  }
+
+  const articles: Promise<CardItem[]> = microcmsClient.v1.blogs
+    .$get({ query })
+    .then((res) => res.contents)
+    .then((contents) => (contents as CardItem[]))
+
+  const slides: Promise<CardItem[]> = microcmsClient.v1.slides
+    .$get({ query })
+    .then((res) => res.contents)
+    .then((contents) => (contents as CardItem[]))
+
+  const site: Promise<Site> = microcmsClient.v1.site
+    .$get()
+
+  const props = await Promise.all([category, articles, slides, site]).then(
+    ([category, articles, slides, site]) => ({ category, articles, slides, site })
+  )
+
+  return {
+    props: props,
+    revalidate: 10,
+  }
+}
---
+++ src/pages/categories/[slug].page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'; React
+import type { NextPage } from 'next'
+import Meta from '@/components/meta'; Meta
+import CardList from '@/components/card-list'; CardList
+import WithSidebar from '@/components/layouts/with-sidebar'; WithSidebar
+import type { Props } from './[slug].hook'
+
+const CategoryPage: NextPage<Props> = ({
+  category,
+  articles,
+  slides,
+  site,
+}) => pug`
+  Meta(title=(category + ' | '  + site.title), desc=category, kw=category)
+  WithSidebar(site=site)
+    section.uk-section.uk-sextion-small.uk-background-primary.uk-flex.uk-flex-center.uk-margin-medium-bottom
+      h4.uk-text-lead.uk-text-break #[span(data-uk-icon='tag').uk-margin-small-right] #{category.name}
+
+    if articles.length
+      CardList(basePath='/blogs', items=articles)
+
+    if slides.length
+      CardList(basePath='/slides', items=slides)
+`
+
+export { getStaticPaths, getStaticProps } from './[slug].hook'
+export default CategoryPage
