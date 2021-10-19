7b5d1454e0ddc5f76d8b80cb8c4612448fffc9b4
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-10-19 13:02:14 +0900

    :sparkles: Setup headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
+const securityHeaders = [
+  { key: 'X-DNS-Prefetch-Control', value: 'on' },
+  { key: 'X-XSS-Protection', value: '1; mode=block' },
+  { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+  { key: 'X-Content-Type-Options', value: 'nosniff' },
+  { key: 'Referrer-Policy', value: 'same-origin' },
+]
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  poweredByHeader: false,
+
+  headers: async () => [
+    { source: '/(.*)', headers: securityHeaders },
+  ],
 
   webpack: (config, { isServer }) => {
     // load worker files as a urls by using Asset Modules
