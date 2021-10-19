af4adde50f6e82b3330aa9b6dc015c34c484af39
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-10-19 11:13:33 +0900

    :white_check_mark: Add CardList to /slides

--- src/pages/slides/index.page.tsx
+++ src/pages/slides/index.page.tsx
@@ -1,14 +1,15 @@
 import React from 'react'; React
 import type { NextPage } from 'next'
 import Meta from '@/components/meta'; Meta
+import CardList from '@/components/card-list'; CardList
 import WithSidebar from '@/components/layouts/with-sidebar'; WithSidebar
 import type { Props } from './index.hook'
 
-const SlidesPage: NextPage<Props> = ({ site }) => pug`
+const SlidesPage: NextPage<Props> = ({ items, site }) => pug`
   - const kw = site.categories.map((cat) => cat.name).join(', ')
   Meta(title=('slides | ' + site.title), desc=site.description, kw=kw)
   WithSidebar(site=site)
-    .uk-container.uk-container-expand.uk-background-muted
+    CardList(basePath='/slides', items=items)
 `
 
 export { getStaticProps } from './index.hook'
