5f4aeb3de6d4622fd128b509420f294dcba34a77
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-10-22 22:05:09 +0900

    :white_check_mark: Add buid time data

--- src/components/layouts/children/footer.tsx
+++ src/components/layouts/children/footer.tsx
@@ -1,3 +1,5 @@
+import DateTime from '@/lib/date-time'; DateTime
+
 interface Props {
   author: Author
 }
@@ -6,6 +8,9 @@ const Footer: React.VFC<Props> = ({ author }) => pug`
   .uk-section.uk-section-default.uk-section-small.uk-margin-remove-bottom
     p.uk-text-center.uk-text-meta
       | Copyright #[span &copy;]2021 #[a(href=author.accounts.twitter, target='_blank') #{author.name}]. All rights reserved.
+    p.uk-text-center.uk-text-meta
+      - const time= (new Date()).toISOString()
+      | Revised at #[time(dateTime=time) #{DateTime.date(time)}]
 `
 
 export default Footer
