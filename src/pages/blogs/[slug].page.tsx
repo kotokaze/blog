129949bcf76fb1c2bed44ac4beb7ccd7bb32017a
Kotokaze <62094392+kotokaze@users.noreply.github.com>
2021-12-04 13:48:10 +0900

    master (#19)
    * dev (#17)
    
    * :white_check_mark: Simplify the expression
    
    * :memo: Update readme and add architecture figure
    
    * :memo: Add api endpoints
    
    * [ImgBot] Optimize images
    
    /docs/src/architecture.png -- 137.58kb -> 104.18kb (24.28%)
    
    Signed-off-by: ImgBotApp <ImgBotHelp@gmail.com>
    
    Co-authored-by: ImgBotApp <ImgBotHelp@gmail.com>

--- src/pages/blogs/[slug].page.tsx
+++ src/pages/blogs/[slug].page.tsx
@@ -19,14 +19,8 @@ const BlogPage: NextPage<Props> = ({ article, site, preview }) => {
         .uk-flex.uk-flex-center
           h5.uk-text-lead #{article.subTitle}
         .uk-flex.uk-flex-center.uk-grid-column-medium(data-uk-grid)
-          if article.publishedAt
-            p #[span(data-uk-icon='calendar')] #[time(dateTime=article.publishedAt) #{DateTime.date(article.publishedAt)}]に公開
-          else
-            p #[span(data-uk-icon='calendar')] #[time(dateTime=article.createdAt) #{DateTime.date(article.createdAt)}]に作成
-          if article.revisedAt
-            p #[span(data-uk-icon='history')] #[= DateTime.elapsed(article.revisedAt)]前に更新
-          else
-            p #[span(data-uk-icon='history')] #[= DateTime.elapsed(article.publishedAt || article.createdAt)]前に更新
+          p #[span(data-uk-icon='calendar')] #[time(dateTime=article.publishedAt || article.createdAt) #{DateTime.date(article.publishedAt || article.createdAt)}]に公開
+          p #[span(data-uk-icon='history')] #[= DateTime.elapsed(article.revisedAt || article.publishedAt || article.createdAt)]前に更新
 
       .uk-margin-medium-bottom
         p.uk-text-meta #[span(data-uk-icon='tag')] Tags: #[span &nbsp;]
