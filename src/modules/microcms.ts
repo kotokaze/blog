d46fde7a7668d96584b0cde426695e680138ce91
kotokaze <62094392+kotokaze@users.noreply.github.com>
2021-12-08 02:39:24 +0900

    :warning: Avoid 5xx error with axios

--- src/modules/microcms.ts
+++ src/modules/microcms.ts
@@ -5,6 +5,7 @@ import api from "@/api/$api"
 const axiosConfig: AxiosRequestConfig = {
   baseURL: process.env.MICROCMS_API_HOST!,
   headers: { 'X-API-KEY': process.env.MICROCMS_API_KEY! },
+  validateStatus: (status) => (200 <= status && status < 300) || status == 404,
 }
 
 const microcmsClient = api(aspida(axios, axiosConfig))
