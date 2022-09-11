99900ce6f76cf6d3de09bcf7db4506e5b0fe60ac
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-09-11 14:32:29 +0900

    :white_check_mark: Separate the slug check

--- src/pages/api/preview/index.hook.ts
+++ src/pages/api/preview/index.hook.ts
@@ -4,10 +4,25 @@ const PageType: { [key: string]: string } = {
   BLOGS: 'blogs',
   INFO: 'info',
 }
+type PageType = typeof PageType[keyof typeof PageType]
 
 type ErrCode = {
   code: number,
   message: string,
+}
+
+const isSlugExists = async (type: PageType, slug: string, draftKey: string): Promise<boolean> => {
+  if (type === PageType.BLOGS) {
+    const data: { id: string } = await microcmsClient.v1.blogs._id(slug).$get({
+      query: {
+        fields: 'id',
+        draftKey: draftKey.toString(),
+      },
+    })
+    return !!data.id
+  }
+
+  return false
 }
 
 const validateQuery = async (query: { [key: string]: string | string[] }): Promise<{
@@ -23,13 +38,13 @@ const validateQuery = async (query: { [key: string]: string | string[] }): Promi
   if (query.type !== PageType.INFO) {
     if (!query.slug) return { err: { code: 401, message: 'Invalid request' } }
 
-    const data: { id: string } = await microcmsClient.v1.blogs._id(`${query.slug}`).$get({
-      query: {
-        fields: 'id',
-        draftKey: query.draftKey.toString(),
-      },
-    })
-    if (!data) return { err: { code: 404, message: 'Invalid slug' } }
+    if (!
+      await isSlugExists(
+        Object.values(PageType).find((v) => v === query.type.toString()) as PageType,
+        query.slug.toString(),
+        query.draftKey.toString(),
+      )
+    ) return { err: { code: 404, message: 'Invalid slug' } }
   }
 
   return {}
