09673c58f624d2553a6d55dd85e232db75c08959
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-09-26 13:49:15 +0900

    :white_check_mark: Refactor front design

--- src/components/layouts/children/footer.tsx
+++ src/components/layouts/children/footer.tsx
@@ -7,7 +7,7 @@ interface Props {
 const Footer: React.FC<Props> = ({ author }) => pug`
   .uk-section.uk-section-default.uk-section-small.uk-margin-remove-bottom
     p.uk-text-center.uk-text-meta
-      | Copyright #[span &copy;]2021 #[a(href=('https://twitter.com/' + author.accounts.twitter), target='_blank') #{author.name}]. All rights reserved.
+      | Copyright #[span &copy;]2021 - 2022 #[a(href=('https://twitter.com/' + author.accounts.twitter), target='_blank') #{author.name}]. All rights reserved.
 `
 
 export default Footer
