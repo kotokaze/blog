0f130cbfe216b74c5156f78188615d326b5a4a25
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-09-26 09:29:38 +0900

    :white_check_mark: Use account name instead of Url

--- src/pages/info/index.page.tsx
+++ src/pages/info/index.page.tsx
@@ -32,33 +32,28 @@ const InfoPage: NextPage<Props> = ({ site, preview }) => {
           ul
             if site.author.accounts.github
               li
-                Link(href=site.author.accounts.github)
+                Link(href=('https://github.com/' + site.author.accounts.github))
                   a(target='_blank') GitHub
 
             if site.author.accounts.twitter
               li
-                Link(href=site.author.accounts.twitter)
+                Link(href=('https://twitter.com/' + site.author.accounts.twitter))
                   a(target='_blank') Twitter
 
             if site.author.accounts.zenn
               li
-                Link(href=site.author.accounts.zenn)
+                Link(href=('https://zenn.dev/' + site.author.accounts.zenn))
                   a(target='_blank') Zenn
 
             if site.author.accounts.qiita
               li
-                Link(href=site.author.accounts.qiita)
+                Link(href='https://qiita.com/' + site.author.accounts.qiita)
                   a(target='_blank') Qiita
 
             if site.author.accounts.speakerdeck
               li
-                Link(href=site.author.accounts.speakerdeck)
+                Link(href='https://speakerdeck.com/' + site.author.accounts.speakerdeck)
                   a(target='_blank') Speaker Deck
-
-            if site.author.accounts.wordpress
-              li
-                Link(href=site.author.accounts.wordpress)
-                  a(target='_blank') WordPress
   `
 }
 
