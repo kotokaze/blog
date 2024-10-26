1eb421ad8217fb4d80174e9c2188cba00ec08ac8
kotokaze <62094392+kotokaze@users.noreply.github.com>
2024-10-27 00:28:49 +0900

    :sparkles: Add revalidate route

--- src/app/api/draft/lib.ts
+++ src/app/api/draft/lib.ts
@@ -1,21 +1,16 @@
+import { PageKind } from '@/app/api/lib';
 import {
   getArticleBySlug,
   getSlideBySlug,
 } from '@/lib/microcms';
-import { NextResponse } from 'next/server';
-import { type Status } from '../lib';
-
-export enum PageKind {
-  INFO = 'info',
-  ARTICLES = 'articles',
-  SLIDES = 'slides',
-}
 
 export const isValidSlug = async (type: PageKind, slug: string, dk: string): Promise<boolean> => {
   if (type === PageKind.ARTICLES) {
     const promise = getArticleBySlug(slug, {
       queries: { fields: 'id', draftKey: dk },
-    }).then((_) => true).catch(() => false);
+    })
+      .then((_) => true)
+      .catch(() => false);
 
     return await promise;
   }
@@ -23,21 +18,12 @@ export const isValidSlug = async (type: PageKind, slug: string, dk: string): Pro
   if (type === PageKind.SLIDES) {
     const promise = getSlideBySlug(slug, {
       queries: { fields: 'id', draftKey: dk },
-    }).then((_) => true).catch(() => false);
+    })
+      .then((_) => true)
+      .catch(() => false);
 
     return await promise;
   }
 
   return false;
-};
-
-export const jsonResponse = (body: Status): NextResponse<Status> => {
-  const res: ResponseInit = {};
-  res.headers = new Headers({
-    'Content-Type': 'application/json',
-  });
-  res.status = body.code;
-  res.statusText = body.message;
-
-  return new NextResponse<Status>(JSON.stringify(body), res);
 };
--- src/app/api/lib.ts
+++ src/app/api/lib.ts
@@ -1,4 +1,30 @@
+import { NextResponse } from 'next/server';
+
+export enum PageKind {
+  INFO = 'info',
+  ARTICLES = 'articles',
+  SLIDES = 'slides',
+}
+
 export interface Status {
   readonly code: number;
   readonly message: string;
 }
+
+export const succeeded: Status = { code: 200, message: 'OK' };
+
+export const badRequest: Status = { code: 400, message: 'Bad Request' };
+export const unauthorized: Status = { code: 401, message: 'Unauthorized' };
+export const notFound: Status = { code: 404, message: 'Not Found' };
+export const internalServerError: Status = { code: 500, message: 'Internal Server Error' };
+
+export const jsonResponse = (body: Status): NextResponse<Status> => {
+  const res: ResponseInit = {};
+  res.headers = new Headers({
+    'Content-Type': 'application/json',
+  });
+  res.status = body.code;
+  res.statusText = body.message;
+
+  return new NextResponse<Status>(JSON.stringify(body), res);
+};
