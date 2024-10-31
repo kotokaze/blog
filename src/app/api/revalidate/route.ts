01a63237858a669b816299e0b622f79b680291c3
kotokaze <62094392+kotokaze@users.noreply.github.com>
2024-10-31 22:47:23 +0900

    :white_check_mark: Fix revalidate route algorithm

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -17,23 +17,43 @@ import {
 } from '@/app/api/lib';
 
 export const dynamic = 'force-dynamic';
-export const revalidate = 'false';
+export const revalidate = false;
 export const POST = async (req: NextRequest): Promise<NextResponse<Status>> => {
   if (process.env.REVALIDATE_SECRET == null) {
     return jsonResponse(internalServerError);
   }
 
+  const signature = req.headers.get('X-MICROCMS-Signature') ?? '';
+  if (!signature) {
+    return jsonResponse(badRequest);
+  }
+
   const hmac = createHmac('sha256', process.env.REVALIDATE_SECRET);
   const body = await req.text();
   const expected = hmac.update(body).digest('hex');
-  const signature = req.headers.get('X-MICROCMS-Signature') ?? '';
-  if (!timingSafeEqual(Buffer.from(signature), Buffer.from(expected))) {
-    return jsonResponse(unauthorized);
+
+  try {
+    if (!timingSafeEqual(Buffer.from(signature), Buffer.from(expected))) {
+      return jsonResponse(unauthorized);
+    }
+  }
+ catch {
+    return jsonResponse(internalServerError);
   }
 
-  const { searchParams } = new URL(req.url);
-  const path = decodeURIComponent(searchParams.get('path') ?? '');
+  const basePathMap = {
+    site: '/info',
+    blogs: '/posts',
+    slides: '/slides',
+  };
 
+  const json = JSON.parse(body) as {
+    id?: string;
+    api: keyof typeof basePathMap;
+    type: 'new' | 'edit' | 'delete';
+  };
+
+  const path = basePathMap[json.api] + (json.id != null ? `/${json.id}` : '');
   if (!path) {
     return jsonResponse(badRequest);
   }
@@ -41,7 +61,8 @@ export const POST = async (req: NextRequest): Promise<NextResponse<Status>> => {
   // Revalidate the segments recursively
   const segments = path.split('/').filter((seg) => seg.length);
   for (let i = segments.length; 0 < i; i--) {
-    const target = `/${segments.slice(0, i).join('/')}`
+    const target = `/${segments.slice(0, i).join('/')}`;
+    console.log('revalidatePath: ', target);
     revalidatePath(target, 'page');
   }
 
